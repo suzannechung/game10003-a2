b66267bf38ab26a752e60f861f4ae87ab4ff2dae
Suzanne Chung <suzanne.chung@mohawkcollege.ca>
2025-01-28 23:21:15 -0500

    colouring book - colour palette

--- chung-suzanne-a2-drawing/Game.cs
+++ chung-suzanne-a2-drawing/Game.cs
@@ -11,14 +11,23 @@ namespace MohawkGame2D
     public class Game
     {
         // Place your variables here:
-
+        Color lightbrown = new Color(197,163,121);
+        Color brown = new Color(154, 102, 64);
+        Color beige = new Color(240, 207, 167);
+        Color stone = new Color(229, 223, 207);
+        Color snow = new Color(244, 243, 237);
 
         /// <summary>
         ///     Setup runs once before the game loop begins.
         /// </summary>
         public void Setup()
         {
-
+            // Set up window
+            Window.SetTitle("Colouring Book: Bunny");
+            Window.SetSize(800, 600);
+            // Remove outlines
+            Draw.LineColor = Color.Clear;
+        
         }
 
         /// <summary>
@@ -26,7 +35,24 @@ namespace MohawkGame2D
         /// </summary>
         public void Update()
         {
+            // Reset background
+            //Window.ClearBackground(Color.OffWhite);
 
+            //create colour palette for colouring
+            Draw.FillColor = lightbrown;
+            Draw.Rectangle(10, 10, 50, 50);
+
+            Draw.FillColor = brown;
+            Draw.Rectangle(70, 10, 50, 50);
+            
+            Draw.FillColor = beige;
+            Draw.Rectangle(130, 10, 50, 50);
+
+            Draw.FillColor = stone;
+            Draw.Rectangle(190, 10, 50, 50);
+
+            Draw.FillColor = snow;
+            Draw.Rectangle(250, 10, 50, 50);
         }
     }
 
