8c6c461ad226dd5214e9ab9bedb6ec629db4a39c
Suzanne Chung <suzanne.chung@mohawkcollege.ca>
2025-01-29 01:04:12 -0500

    clouds

--- chung-suzanne-a2-drawing/Game.cs
+++ chung-suzanne-a2-drawing/Game.cs
@@ -1,6 +1,7 @@
 ﻿// Include the namespaces (code libraries) you need below.
 using System;
 using System.Numerics;
+using System.Reflection.Metadata;
 
 // The namespace your code is in.
 namespace MohawkGame2D
@@ -11,11 +12,9 @@ namespace MohawkGame2D
     public class Game
     {
         // Place your variables here:
-        Color lightbrown = new Color(197,163,121);
-        Color brown = new Color(154, 102, 64);
-        Color beige = new Color(240, 207, 167);
-        Color stone = new Color(229, 223, 207);
-        Color snow = new Color(244, 243, 237);
+        //Color water = new Color("#88D2F0");
+        //Color darksand = new Color("#CE9963");
+        Color cloud = new Color(197, 226, 247);
 
         /// <summary>
         ///     Setup runs once before the game loop begins.
@@ -23,7 +22,7 @@ namespace MohawkGame2D
         public void Setup()
         {
             // Set up window
-            Window.SetTitle("Colouring Book: Bunny");
+            Window.SetTitle("Weather Changer");
             Window.SetSize(800, 600);
             // Remove outlines
             Draw.LineColor = Color.Clear;
@@ -36,23 +35,21 @@ namespace MohawkGame2D
         public void Update()
         {
             // Reset background
-            //Window.ClearBackground(Color.OffWhite);
+            Window.ClearBackground(Color.OffWhite);
 
             //create colour palette for colouring
-            Draw.FillColor = lightbrown;
-            Draw.Rectangle(10, 10, 50, 50);
+            Draw.FillColor = cloud;
+            for (int i=0; i <3; i++)
+            {
+                int x1 = 40 + i * 50;
+                Draw.Circle(x1, 100, 50);
 
-            Draw.FillColor = brown;
-            Draw.Rectangle(70, 10, 50, 50);
-            
-            Draw.FillColor = beige;
-            Draw.Rectangle(130, 10, 50, 50);
+                int x2 = 250 + i * 50;
+                Draw.Circle(x2, 200, 50);
 
-            Draw.FillColor = stone;
-            Draw.Rectangle(190, 10, 50, 50);
-
-            Draw.FillColor = snow;
-            Draw.Rectangle(250, 10, 50, 50);
+                int x3 = 500 + i * 50;
+                Draw.Circle(x3, 150, 50);
+            }
         }
     }
 
